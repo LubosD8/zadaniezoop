67f83188c3e116c7736185d0405a00c68fa1f99e
LubosD8 <LubosD8@github.com>
2017-11-12 15:57:04 +0100

    Zmena podmienky vypisu pripojenych zariadeni

--- src/Main.java
+++ src/Main.java
@@ -14,9 +14,8 @@ public class Main {
         String a;
         int b;
         System.out.println("Cloned mouse ID "+processor.newMouse.id+"Cloned mouse Status :"+ processor.newMouse.status);
-        processor.connected();
         System.out.println("What did you do?");
-        System.out.println("1- Right Click 2- Left Click  3- Middle wheel 4- Print memory values 5- Press keys on keyboard 6- USB handling");
+        System.out.println("1- Right Click 2- Left Click  3- Middle wheel 4- Print memory values 5- Press keys on keyboard 6- USB handling 7- What devices are connected");
         while ((typeOfCase = scanner.nextInt()) != 0) {
             switch (typeOfCase) {
                 case 1:
@@ -45,6 +44,10 @@ public class Main {
                     System.out.println("If you want to transfer to USB type positive number. IF remove type negative number");
                     b= scanner.nextInt();
                     processor.transfer(b);
+                    break;
+                case 7:
+                    processor.connected();
+                    break;
                 default:
                     System.out.println("No action found");
                     break;
